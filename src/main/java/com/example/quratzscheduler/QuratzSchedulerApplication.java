ecf937364a2747e52a54fbb2a033ce122e2b9253
ayg08 <ayg@oneplat.co>
2023-12-26 21:08:36 +0900

    초기커밋

---
+++ src/main/java/com/example/quratzscheduler/QuratzSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.quratzscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuratzSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuratzSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quratzscheduler/QuratzSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quratzscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuratzSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
